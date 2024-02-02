e0a300adb9f3c75a2f80ebe3b9c9c062b30c7137
ZEFIR001 <66650548+ZEFIR001@users.noreply.github.com>
2024-02-02 11:14:37 +0300

    Add files via upload

---
+++ modded_assignements.c
@@ -0,0 +1,6 @@
+//letting the game additionally know that our item is clothing
+class zfUnicorn_Kiga_Base extends Clothing_Base {};
+//makes that actual classname of kigurumi are part of base class in scripting enviroment,retexture classnames should be added here.
+//honestly,you can even try to add here any item that you want to hide particular slot which you set in modded_playerbase.
+class zfUnicorn_Kiga_Pink : zfUnicorn_Kiga_Base {};
+class zfUnicorn_Kiga_Yellow : zfUnicorn_Kiga_Base {}; 
